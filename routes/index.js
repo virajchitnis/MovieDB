e6d2a606592a49767261fd2369c681367afffff6
Viraj Chitnis <chitnisviraj@gmail.com>
2015-01-31 09:20:16 -0500

    Added login page and app details
    Added a login page to views and added application details to package.json.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var pjson = require('../package.json');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Movies - ' + pjson.name });
+});
+
+/* GET login page. */
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Login - ' + pjson.name });
 });
 
 module.exports = router;
