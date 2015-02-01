6a612e20c9656eec8fc9cf27b18fc16b822830b0
Viraj Chitnis <chitnisviraj@gmail.com>
2015-02-01 11:48:13 -0500

    Unnecessary info is hidden during GET requests
    When movie data is requested via GET, unnecessary info such as '_id', and '__v' fields are hidden.

--- routes/movies.js
+++ routes/movies.js
@@ -20,7 +20,35 @@ router.param('movie', function(req, res, next, imdb_id) {
 router.get('/', function(req, res) {
 	Movie.find(function (err, movies) {
 		if (err) return next(err);
-		res.json(movies);
+		
+		var json = new Array();
+		for (var i = 0; i < movies.length; i++) {
+			var movie = {
+				imdb_id: movies[i].imdb_id,
+				title: movies[i].title,
+				year: movies[i].year,
+				rated: movies[i].rated,
+				released: movies[i].released,
+				runtime: movies[i].runtime,
+				genre: movies[i].genre,
+				director: movies[i].director,
+				writer: movies[i].writer,
+				actors: movies[i].actors,
+				plot: movies[i].plot,
+				language: movies[i].language,
+				country: movies[i].country,
+				awards: movies[i].awards,
+				poster: movies[i].poster,
+				metascore: movies[i].metascore,
+				imdb_rating: movies[i].imdb_rating,
+				imdb_votes: movies[i].imdb_votes,
+				quality: movies[i].quality,
+				filename: movies[i].filename
+			};
+			json.push(movie);
+		}
+		
+		res.json(json);
 	});
 });
 
@@ -73,7 +101,30 @@ router.post('/', function(req, res, next) {
 
 /* GET a particular movie by its id */
 router.get('/:movie', function(req, res) {
-	res.json(req.movie);
+	var movie = {
+		imdb_id: req.movie.imdb_id,
+		title: req.movie.title,
+		year: req.movie.year,
+		rated: req.movie.rated,
+		released: req.movie.released,
+		runtime: req.movie.runtime,
+		genre: req.movie.genre,
+		director: req.movie.director,
+		writer: req.movie.writer,
+		actors: req.movie.actors,
+		plot: req.movie.plot,
+		language: req.movie.language,
+		country: req.movie.country,
+		awards: req.movie.awards,
+		poster: req.movie.poster,
+		metascore: req.movie.metascore,
+		imdb_rating: req.movie.imdb_rating,
+		imdb_votes: req.movie.imdb_votes,
+		quality: req.movie.quality,
+		filename: req.movie.filename
+	};
+	
+	res.json(movie);
 });
 
 module.exports = router;
